b6f9f05014ce08238babe03d03f42584876e6d42
IselDrew <andryshonochek0604@gmail.com>
2019-12-15 00:13:17 +0200

    Add footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,25 @@
-import React from "react"
-import footerLogo from "../images/footerLogo.png"
+import React from "react";
+import footerLogo from "../images/footerLogo.png";
+
+import "../styles/footer.scss";
 
 function Footer() {
   return (
-    <div>
-      <div>
+    <div className="footer">
+      <div className="footer__top">
         <img src={footerLogo} alt="My Letter Logo"></img>
-        <span>Private Policy</span>
-        <hr />
-        <span>Term Of Use</span>
+        <div className='footer__top--text'>
+          <span>Private Policy</span>
+          <div></div>
+          <span>Term Of Use</span>
+        </div>
       </div>
 
-      <div>
+      <div className="footer__bottom">
         <span>© 2019, My Letter. All rights reserved.</span>
       </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
