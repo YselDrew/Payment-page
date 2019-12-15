1f3b6a333ee00305b1f72dac90125bfdad76b992
IselDrew <andryshonochek0604@gmail.com>
2019-12-15 23:23:20 +0200

    Add second style to components

--- src/components/AnnualForm.js
+++ src/components/AnnualForm.js
@@ -2,9 +2,11 @@ import React from "react";
 import AnnualOffer from "./AnnualOffer.js";
 import FormComponent from "./FormComponent";
 
+import '../styles/components-containers.scss'
+
 function TrialForm(props) {
   return (
-    <div>
+    <div className='annual-form-container'>
       <AnnualOffer openOffer={props.openOffer} isOpen={props.isOfferOpen} />
       <FormComponent />
     </div>
--- src/components/AnnualOffer.js
+++ src/components/AnnualOffer.js
@@ -9,22 +9,46 @@ function AnnualPlanOffer(props) {
   const { isOpen, openOffer } = props;
 
   return (
-    <div className="annual-container">
-      <div className='annual-content'>
+    <div className={isOpen ? "opened-annual-container" : "annual-container"}>
+      <div className={isOpen ? "opened-annual-content" : "annual-content"}>
         <img src={annualImg} alt="#"></img>
-        <span className='annual-content__title'>Annual plan</span>
-        <span className='annual-content__price'>
+        <span
+          className={
+            isOpen ? "opened-annual-content__title" : "annual-content__title"
+          }
+        >
+          Annual plan
+        </span>
+        <span
+          className={
+            isOpen ? "opened-annual-content__price" : "annual-content__price"
+          }
+        >
           $39.00<span>/ year</span>
         </span>
-        <span className='annual-content__text'>Your Premium plan will auto-renew. Cancel any time.</span>
+        <span
+          className={
+            isOpen ? "opened-annual-content__text" : "annual-content__text"
+          }
+        >
+          Your Premium plan will auto-renew. Cancel any time.
+        </span>
       </div>
       {isOpen ? (
         <Link to="/" onClick={openOffer}>
-          <input type="button" value="Change"></input>
+          <input
+            className="annual-input__close"
+            type="button"
+            value="Change"
+          ></input>
         </Link>
       ) : (
         <Link to="/annual" onClick={openOffer}>
-          <input className='annual-input__open' type="button" value="Get Started"></input>
+          <input
+            className="annual-input__open"
+            type="button"
+            value="Get Started"
+          ></input>
         </Link>
       )}
     </div>
